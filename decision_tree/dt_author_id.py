157a01a0262d887a44dac8ffd173c7b31d0f49a7
Ashwin Nikam <anikam@buffalo.edu>
2017-12-18 16:35:33 -0500

    did decision trees mini project

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -12,7 +12,7 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn.tree import DecisionTreeClassifier
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -25,6 +25,10 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+print len(features_train[0])
+clf = DecisionTreeClassifier(min_samples_split = 40)
+clf.fit(features_train, labels_train)
+print clf.score(features_test, labels_test)
 
 #########################################################
 
