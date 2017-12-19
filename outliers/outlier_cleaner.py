46680b531fbe84726ab52e80fdfc3b5a36964923
Ashwin Nikam <anikam@buffalo.edu>
2017-12-19 14:47:24 -0500

    outlier removal done

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -12,9 +12,29 @@ def outlierCleaner(predictions, ages, net_worths):
     """
     
     cleaned_data = []
+    error_list = []
 
     ### your code goes here
+    for i in range(len(predictions)):
+        prediction = predictions[i][0]
+        net_worth = net_worths[i][0]
+        error = net_worth - prediction
+        error_list.append(error)
 
+    error_list.sort()
+    size = len(predictions)
+    n = 0.9 * size
+    x = size - n
+    error_list = error_list[:int(x)]
+    
+    for i in range(len(predictions)):
+        prediction = predictions[i][0]
+        net_worth = net_worths[i][0]
+        age = ages[i][0]
+        error = net_worth - prediction
+        if error not in error_list:
+            my_tuple = (age, net_worth, error)
+            cleaned_data.append(my_tuple)
     
     return cleaned_data
 
